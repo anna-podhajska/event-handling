c9116a876a8a4a80ab84a6107923171c13d7abac
Anna P <podhajskaa@gmail.com>
2017-05-31 18:05:31 +1200

    green done

--- events.js
+++ events.js
@@ -1,5 +1,5 @@
-// Don't change or delete this line! It waits until the DOM has loaded, then calls 
-// the start function. More info: 
+// Don't change or delete this line! It waits until the DOM has loaded, then calls
+// the start function. More info:
 // https://developer.mozilla.org/en-US/docs/Web/Events/DOMContentLoaded
 document.addEventListener('DOMContentLoaded', start)
 
@@ -7,7 +7,10 @@ function start () {
   // The first example is done for you. This will change the background colour of the first div
   // when you mouse over it.
   one()
-    
+  two()
+  three()
+  four()
+
   // Your turn! Create a new function called `two`, then call it from here.
 }
 
@@ -17,22 +20,43 @@ function one () {
 
   // Next, we add an event listener to it:
   one.addEventListener('mouseenter', makeBlue)
-
   // Finally, we add one to make the colour white again
   one.addEventListener('mouseleave', makeWhite)
 }
 
 // CREATE FUNCTION two HERE
-
+function two () {
+  var one = document.getElementById('two');
+  one.addEventListener('mouseenter', makeGreen);
+  one.addEventListener('mouseleave', makeWhite);
+}
 // CREATE FUNCTION three HERE
+function three () {
+  var one = document.getElementById('three');
+  one.addEventListener('mouseenter', makeGray);
+  one.addEventListener('mouseleave', makeWhite);
+}
 
 // CREATE FUNCTION four HERE
+function four () {
+  var one = document.getElementById('four');
+  one.addEventListener('mouseenter', makeBlack);
+  one.addEventListener('mouseleave', makeWhite);
+}
 
 // Changes the background color of event's target
 function makeBlue (evt) {
   evt.target.style.backgroundColor = 'blue'
 }
-
 function makeWhite (evt) {
   evt.target.style.backgroundColor = 'white'
+}
+function makeGreen (evt) {
+  evt.target.style.backgroundColor = 'green'
+}
+function makeBlack (evt) {
+  evt.target.style.backgroundColor = 'black'
+}
+function makeGray (evt) {
+  evt.target.style.backgroundColor = 'gray'
 }
