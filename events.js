1d50329498861562b6ebfe8126d8f3a93857bf56
Anna P <podhajskaa@gmail.com>
2017-05-31 18:25:46 +1200

    added function refactoring - all functions in one

--- events.js
+++ events.js
@@ -6,43 +6,42 @@ document.addEventListener('DOMContentLoaded', start)
 function start () {
   // The first example is done for you. This will change the background colour of the first div
   // when you mouse over it.
-  one()
-  two()
-  three()
-  four()
+  one('one', makeBlue, makeWhite);
+  one('two', makeGreen, makeWhite);
+  one('three', makeOrange, makeWhite);
+  one('four', makeBlack, makeWhite);
 
   // Your turn! Create a new function called `two`, then call it from here.
 }
 
-function one () {
+function one (elementName, colorOn, colorOff) {
   // First, we have to find the element:
-  var one = document.getElementById('one')
-
+  var one = document.getElementById(elementName)
   // Next, we add an event listener to it:
-  one.addEventListener('mouseenter', makeBlue)
+  one.addEventListener('mouseenter', colorOn)
   // Finally, we add one to make the colour white again
-  one.addEventListener('mouseleave', makeWhite)
+  one.addEventListener('mouseleave', colorOff)
 }
-
-// CREATE FUNCTION two HERE
-function two () {
-  var one = document.getElementById('two');
-  one.addEventListener('mouseenter', makeGreen);
-  one.addEventListener('mouseleave', makeWhite);
-}
-// CREATE FUNCTION three HERE
-function three () {
-  var one = document.getElementById('three');
-  one.addEventListener('mouseenter', makeOrange);
-  one.addEventListener('mouseleave', makeWhite);
-}
-
-// CREATE FUNCTION four HERE
-function four () {
-  var one = document.getElementById('four');
-  one.addEventListener('mouseenter', makeBlack);
-  one.addEventListener('mouseleave', makeWhite);
-}
+//
+// // CREATE FUNCTION two HERE
+// function two () {
+//   var one = document.getElementById('two');
+//   one.addEventListener('mouseenter', makeGreen);
+//   one.addEventListener('mouseleave', makeWhite);
+// }
+// // CREATE FUNCTION three HERE
+// function three () {
+//   var one = document.getElementById('three');
+//   one.addEventListener('mouseenter', makeOrange);
+//   one.addEventListener('mouseleave', makeWhite);
+// }
+//
+// // CREATE FUNCTION four HERE
+// function four () {
+//   var one = document.getElementById('four');
+//   one.addEventListener('mouseenter', makeBlack);
+//   one.addEventListener('mouseleave', makeWhite);
+// }
 
 // Changes the background color of event's target
 function makeBlue (evt) {
